1c27769326d000dd6d15f7847a1f6fc206a89231
Alessandra <alessandra.fais@gmail.com>
2019-04-04 15:12:31 +0200

    Updated properties and constants

--- SpikeDetection/src/main/java/Constants/BaseConstants.java
+++ SpikeDetection/src/main/java/Constants/BaseConstants.java
@@ -1,27 +1,16 @@
 package Constants;
 
-import org.apache.storm.Config;
-import org.apache.storm.utils.Utils;
-
 /**
- *
- * @author Maycon Viana Bordin <mayconbordin@gmail.com>
+ * Constants useful for all the applications.
  */
 public interface BaseConstants {
-    String BASE_PREFIX = "storm";
-    
-    interface BaseConf {
-        String SPOUT_THREADS     = "%s.spout.threads";
-        String SPOUT_CLASS       = "%s.spout.class";
-        String SPOUT_PATH        = "%s.spout.path";
-        String SPOUT_PARSER      = "%s.spout.parser";
-        
-        String SINK_THREADS        = "%s.sink.threads";
-        String SINK_CLASS          = "%s.sink.class";
-        String SINK_PATH           = "%s.sink.path";
-        String SINK_FORMATTER      = "%s.sink.formatter";
-        
-        String DEBUG_ON = Config.TOPOLOGY_DEBUG;
+    String HELP = "help";
+
+    interface Execution {
+        String LOCAL_MODE = "local";
+        String REMOTE_MODE = "remote";
+        int DEFAULT_RATE = -1;
+        int RUNTIME_SEC = 120;  // topology is alive for 120 seconds
     }
 
     interface BaseComponent {
@@ -30,13 +19,6 @@ public interface BaseConstants {
     }
 
     interface BaseField {
-        String SYSTEMTIMESTAMP = "systemtimestamp";
-        String MSG_ID = "systemmsgID";
-        String TEXT = "text";
-    }
-
-    interface BaseStream {
-        String DEFAULT = Utils.DEFAULT_STREAM_ID;
-        String Marker_STREAM_ID = "marker";
+        String TIMESTAMP = "timestamp";
     }
 }
--- SpikeDetection/src/main/java/Constants/SpikeDetectionConstants.java
+++ SpikeDetection/src/main/java/Constants/SpikeDetectionConstants.java
@@ -1,28 +1,31 @@
 package Constants;
 
 public interface SpikeDetectionConstants extends BaseConstants {
-    String PREFIX = "sd";
-    int max_hz = 80000;
+    String DEFAULT_PROPERTIES = "/spikedetection/sd.properties";
+    String DEFAULT_TOPO_NAME = "SpikeDetection";
+    double DEFAULT_THRESHOLD=0.03d;
     
-    interface Conf extends BaseConf {
+    interface Conf {
+        String SPOUT_PATH = "sd.spout.path";
         String PARSER_VALUE_FIELD = "sd.parser.value_field";
-        String GENERATOR_COUNT = "sd.generator.count";
-        String MOVING_AVERAGE_THREADS = "sd.moving_average.threads";
         String MOVING_AVERAGE_WINDOW = "sd.moving_average.window";
-        String SPIKE_DETECTOR_THREADS = "sd.spike_detector.threads";
         String SPIKE_DETECTOR_THRESHOLD = "sd.spike_detector.threshold";
+
+        String SPOUT_THREADS = "sd.spout.threads";
+        String MOVING_AVERAGE_THREADS = "sd.moving_average.threads";
+        String SPIKE_DETECTOR_THREADS = "sd.spike_detector.threads";
+        String SINK_THREADS = "sd.sink.threads";
+        String ALL_THREADS = "sd.all.threads"; // useful only with Flink
     }
     
     interface Component extends BaseComponent {
-        String MOVING_AVERAGE = "movingAverageBolt";
-        String SPIKE_DETECTOR = "spikeDetectorBolt";
+        String MOVING_AVERAGE = "moving_average";
+        String SPIKE_DETECTOR = "spike_detector";
     }
     
-    interface Field {
+    interface Field extends BaseField {
         String DEVICE_ID = "deviceID";
-        String TIMESTAMP = "timestamp";
         String VALUE = "value";
         String MOVING_AVG = "movingAverage";
-        String MESSAGE = "message";
     }
 }
