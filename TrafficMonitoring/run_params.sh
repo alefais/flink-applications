33b533e40d9958f3a9b5500224d617890ed4fb50
Alessandra <alessandra.fais@gmail.com>
2019-07-11 13:22:09 +0200

    Modified timeout for single test execution

--- TrafficMonitoring/run_params.sh
+++ TrafficMonitoring/run_params.sh
@@ -29,11 +29,8 @@ flink stop $(flink list | grep TrafficMonitoring | awk '{ print $4 }')
 if [ $2 -lt 4 ]
 then
     sleep 1800 # 30 minutes
-elif [ $2 -lt 10 ]
-then
-    sleep 600 # 10 minutes
 else
-    sleep 300 # 5 minutes
+    sleep 600 # 10 minutes
 fi
 
 printf "saving logs...\n"
